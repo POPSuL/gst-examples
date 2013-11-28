7cfe48ed907282b05f5a579ea237d4987f36c7a8
Viktor Suprun <popsul1993@gmail.com>
2013-11-28 21:52:09 +1100

    + example 2 * fixed typo error

--- example2.py
+++ example2.py
@@ -20,15 +20,42 @@ GObject.threads_init()
 def parse_args():
     parser = argparse.ArgumentParser(prog='example1.py')
     parser.add_argument('--volume', help='Указать громкость (0-100)', type=int, default=100)
+    parser.add_argument('--output', help='Путь к файлу в который нужно сохранить поток',
+                        type=str, default='/tmp/out.ogg')
     parser.add_argument('location')
     args = parser.parse_args()
     return args
+
+
+class RecorderBin(Gst.Bin):
+
+    def __init__(self, name=None):
+        super(RecorderBin, self).__init__(name=name)
+
+        self.vorbisenc = Gst.ElementFactory.make("vorbisenc", "vorbisenc")
+        self.oggmux = Gst.ElementFactory.make("oggmux", "oggmux")
+        self.filesink = Gst.ElementFactory.make("filesink", "filesink")
+
+        self.add(self.vorbisenc)
+        self.add(self.oggmux)
+        self.add(self.filesink)
+
+        self.vorbisenc.link(self.oggmux)
+        self.oggmux.link(self.filesink)
+
+        self.sink_pad = Gst.GhostPad.new("sink", self.vorbisenc.get_static_pad("sink"))
+        self.add_pad(self.sink_pad)
+
+    def set_location(self, location):
+        self.filesink.set_property("location", location)
 
 
 class Player():
 
     def __init__(self, args):
         self.pipeline = self.create_pipeline(args)
+
+        self.args = args
 
         ## получаем шину по которой рассылаются сообщения
         ## и вешаем на нее обработчик
@@ -61,6 +88,8 @@ class Player():
         audioconvert = Gst.ElementFactory.make('audioconvert', 'audioconvert')
         volume = Gst.ElementFactory.make('volume', 'volume')
         audiosink = Gst.ElementFactory.make('autoaudiosink', 'autoaudiosink')
+        ## Элемент tee используется для мультиплексирования потока
+        tee = Gst.ElementFactory.make('tee', 'tee')
 
         ## decodebin имеет динамические pad'ы, которые так же динамически
         ## необходимо линковать
@@ -69,17 +98,25 @@ class Player():
         decodebin.connect('pad-added', on_pad_added)
 
         ## добавляем все созданные элементы в pipeline
-        [pipeline.add(k) for k in [source, decodebin, audioconvert, volume, audiosink]]
+        elements = [source, decodebin, audioconvert, volume, audiosink, tee]
+        [pipeline.add(k) for k in elements]
 
         ## линкуем элементы между собой по схеме:
-        ## *src* -> (decodebin + audioconvert) -> volume -> autoaudiosink
+        ##                                               +-> volume -> autoaudiosink
+        ## *src* -> (decodebin + audioconvert) -> tee -> |
+        ##                                             [ +-> vorbisenc -> oggmux -> filesink ]
         source.link(decodebin)
-        audioconvert.link(volume)
+        audioconvert.link_pads('src', tee, 'sink')
+        tee.link_pads('src_0', volume, 'sink')
         volume.link(audiosink)
         return pipeline
 
     def play(self):
         self.pipeline.set_state(Gst.State.PLAYING)
+        recorder = RecorderBin('recorder')
+        self.pipeline.add(recorder)
+        self.pipeline.get_by_name('tee').link_pads('src_1', recorder, 'sink')
+        recorder.set_location(self.args.output)
 
     def message_handler(self, bus, message):
         """Обработчик сообщений"""
